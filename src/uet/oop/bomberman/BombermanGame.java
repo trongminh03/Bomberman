5b80ef7919ffcd0f5e9819c663618b1ef4456f41
nhnamk3 <100186092+nhnamk3@users.noreply.github.com>
2022-09-22 21:30:48 +0700

    update comment

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -9,15 +9,6 @@ import uet.oop.bomberman.gui.MenuViewManager;
 public class BombermanGame extends Application {
     private static Stage stage;
 
-//    public static final int WIDTH = 25;
-//    public static final int HEIGHT = 15;
-//
-//    public static boolean running;
-//
-//    private GraphicsContext gc;
-//    private Canvas canvas;
-//    private List<Entity> entities = new ArrayList<>();
-//    private List<Entity> stillObjects = new ArrayList<>();
 
 
     public static void main(String[] args) {
@@ -30,83 +21,11 @@ public class BombermanGame extends Application {
         MenuViewManager menuView = new MenuViewManager();
         stage.setScene(menuView.getMenuScene());
         stage.setTitle(GlobalConstants.GAME_NAME + GlobalConstants.GAME_VERSION);
-//        stage = menuView.getMenuStage();
         stage.show();
 
-//        // Tao Canvas
-//        canvas = new Canvas(Sprite.SCALED_SIZE * WIDTH, Sprite.SCALED_SIZE * HEIGHT);
-//        gc = canvas.getGraphicsContext2D();
-//
-//        // Tao root container
-//        Group root = new Group();
-//        root.getChildren().add(canvas);
-//
-//        // Tao scene
-//        Scene scene = new Scene(root);
-//
-//        scene.setOnKeyPressed(event -> {
-//            switch (event.getCode()) {
-//                case UP:
-//                    System.out.println("UP");
-//                    break;
-//                case DOWN:
-//                    System.out.println("DOWN");
-//                    break;
-//                case LEFT:
-//                    System.out.println("LEFT");
-//                    break;
-//                case RIGHT:
-//                    System.out.println("RIGHT");
-//                    break;
-//            }
-//        });
-//
-//        // Them scene vao stage
-//        stage.setScene(scene);
-//        stage.setTitle("Bomberman v1.0");
-//        stage.show();
-//
-//        AnimationTimer timer = new AnimationTimer() {
-//            @Override
-//            public void handle(long l) {
-//                render();
-//                update();
-//            }
-//        };
-//        timer.start();
-//
-//        createMap();
-//
-//        Entity bomberman = new Bomber(1, 1, Sprite.player_right.getFxImage());
-//        entities.add(bomberman);
     }
 
     public static void switchScene(Scene scene) {
         stage.setScene(scene);
     }
-//
-//    public void createMap() {
-//        for (int i = 0; i < WIDTH; i++) {
-//            for (int j = 0; j < HEIGHT; j++) {
-//                Entity object;
-//                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
-//                    object = new Wall(i, j, Sprite.wall.getFxImage());
-//                }
-//                else {
-//                    object = new Grass(i, j, Sprite.grass.getFxImage());
-//                }
-//                stillObjects.add(object);
-//            }
-//        }
-//    }
-//
-//    public void update() {
-//        entities.forEach(Entity::update);
-//    }
-//
-//    public void render() {
-//        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-//        stillObjects.forEach(g -> g.render(gc));
-//        entities.forEach(g -> g.render(gc));
-//    }
 }
