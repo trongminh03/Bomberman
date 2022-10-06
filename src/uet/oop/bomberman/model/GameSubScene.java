37be148123ea2a02cb1218b8ad9891b61eb93fe4
Lê Trọng Minh <91895429+trongminh03@users.noreply.github.com>
2022-10-06 20:18:05 +0700

    add subscene menu

---
+++ src/uet/oop/bomberman/model/GameSubScene.java
@@ -0,0 +1,46 @@
+package uet.oop.bomberman.model;
+
+import javafx.animation.TranslateTransition;
+import javafx.scene.Parent;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.SubScene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.util.Duration;
+
+public class GameSubScene extends SubScene {
+    private final static String BACKGROUND_IMAGE = "/model/yellow_panel.png";
+    private boolean isHidden;
+
+    public GameSubScene(Parent parent, double v, double v1, boolean b, SceneAntialiasing sceneAntialiasing) {
+        super(parent, v, v1, b, sceneAntialiasing);
+    }
+
+    public GameSubScene() {
+        super(new AnchorPane(), 320, 320);
+        prefWidth(320);
+        prefHeight(320);
+        BackgroundImage image = new BackgroundImage(new Image(BACKGROUND_IMAGE, 320, 320, false, true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, null);
+        AnchorPane root = (AnchorPane) this.getRoot();
+        root.setBackground(new Background(image));
+        isHidden = true;
+        setLayoutX(640);
+        setLayoutY(130);
+    }
+
+    public void moveSubScene() {
+        TranslateTransition transition = new TranslateTransition();
+        transition.setDuration(Duration.seconds(0.3));
+        transition.setNode(this);
+        if (isHidden) {
+            transition.setToX(-370);
+            isHidden = false;
+        } else {
+            transition.setToX(0);
+            isHidden = true;
+        }
+
+        transition.play();
+    }
+}
