f03ffd3266db2726a5a4e89cd38c30c742d3835a
Lê Trọng Minh <91895429+trongminh03@users.noreply.github.com>
2022-10-19 08:00:04 +0700

    Finish bomberman v1.5

--- src/uet/oop/bomberman/constants/GlobalConstants.java
+++ src/uet/oop/bomberman/constants/GlobalConstants.java
@@ -4,7 +4,7 @@ public class GlobalConstants {
     public static final int WIDTH = 25;
     public static final int HEIGHT = 15;
     public static final String GAME_NAME = "Bomberman";
-    public static final String GAME_VERSION = " v1.3";
+    public static final String GAME_VERSION = " v1.5";
     public static enum GameStatus {
         RUNNING,
         PAUSED,
